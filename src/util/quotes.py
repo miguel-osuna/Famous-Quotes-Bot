b8516613e59239c5cf62dbdff1dad4557199e2e8
Miguel Osuna <osuna.angulo.miguel@gmail.com>
2020-12-23 21:20:48 -0800

    Finish quotes api wrapper

--- src/util/quotes.py
+++ src/util/quotes.py
@@ -45,37 +45,85 @@ class URLs:
         return self.base + self.api_version + self.tags
 
 
+class BearerAuth(requests.auth.AuthBase):
+    """ Custom authentication class for Quotes API. """
+
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, req):
+        req.headers["Authorization"] = "Bearer " + self.token
+        return req
+
+
 class QuotesApi:
+    """ Quotes API Wrapper. """
+
     def __init__(self, api_key):
         self.api_key = api_key
         self.url = URLs()
 
-    def __create_auth(self):
-        """ Creates authentication headers for api key. """
-        headers = {
-            "Authorization": "Bearer {}".format(self.api_key),
-            "Content-Type": "application/json",
-        }
-
-        return headers
-
-    def __get_data(self, url):
+    def __get_data(self, url, payload=None):
         """ Private method that performs a get request. """
-        headers = self.__create_auth()
 
-    def __post_data(self, url):
-        """ Private method that performs a post request. """
-        headers = self.__create_auth()
+        if payload is not None:
+            return requests.get(url, auth=BearerAuth(self.api_key), params=payload)
+        else:
+            return requests.get(url, auth=BearerAuth(self.api_key))
 
-    def __put_data(self, url):
+    def __put_data(self, url, data):
         """ Private method that performs a put request. """
-        headers = self.__create_auth()
+        return requests.put(url, json=data, auth=BearerAuth(self.api_key))
 
-    def __patch_data(self, url):
+    def __patch_data(self, url, data):
         """ Private method that performs a patch request. """
-        headers = self.__create_auth()
+        return requests.put(url, json=data, auth=BearerAuth(self.api_key))
 
     def __delete_data(self, url):
         """ Private method that performs a delete request. """
-        headers = self.__create_auth()
+        return requests.delete(url, auth=BearerAuth(self.api_key))
+
+    def __post_data(self, url, data):
+        """ Private method that performs a post request. """
+        return requests.post(url, json=data, auth=BearerAuth(self.api_key))
+
+    def get_quote(self, quote_id, query_params=None):
+        """ Get quote resource by id. """
+        quote_url = self.url.quote_url() + f"/{quote_id}"
+        return self.__get_data(quote_url, query_params)
+
+    def put_quote(self, quote_id, data):
+        """ Update quote resource by id. """
+        quote_url = self.url.quote_url() + f"/{quote_id}"
+        return self.__put_data(quote_url, data)
+
+    def patch_quote(self, quote_id, data):
+        """ Patch quote resource by id. """
+        quote_url = self.url.quote_url() + f"/{quote_id}"
+        return self.__patch_data(quote_url, data)
+
+    def delete_quote(self, quote_id):
+        """ Delete quote resource by id. """
+        quote_url = self.url.quote_url() + f"/{quote_id}"
+        return self.__delete_data(quote_url)
+
+    def post_quote(self, data):
+        """ Creates quote resource. """
+        quotes_url = self.url.quotes_url()
+        return self.__post_data(quotes_url, data)
+
+    def get_all_quotes(self, query_params=None):
+        """ Gets list of quote resources. """
+        quotes_url = self.url.quotes_url()
+        return self.__get_data(quotes_url, query_params)
+
+    def get_all_authors(self, query_params=None):
+        """ Gets list of author resources. """
+        authors_url = self.url.authors_url()
+        return self.__get_data(authors_url, query_params)
+
+    def get_all_tags(self):
+        """ Gets list of tag resources. """
+        tags_url = self.url.tags_url()
+        return self.__get_data(tags_url)
 
